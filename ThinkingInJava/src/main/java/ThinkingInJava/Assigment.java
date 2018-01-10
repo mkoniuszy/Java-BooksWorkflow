33d9cb3ae9c79f1dc1743c46215bed46096db212
koniu <dupa>
2018-01-10 19:14:37 +0100

    rozdział trzeci 96.

--- ThinkingInJava/src/main/java/ThinkingInJava/Assigment.java
+++ ThinkingInJava/src/main/java/ThinkingInJava/Assigment.java
@@ -12,9 +12,9 @@ public class Assigment {
         t2.level = 47;
         System.out.println("1:t1.level : " +t1.level + ",t2.level : " + t2.level);
         t1 = t2;
-        System.out.println("2.t1.level : " + t1.level + ",t2.level :" + t2.level );
+        System.out.println("2.t1.level : " + t1.level + ",t2.level :" + t2.level);
         t1.level = 27;
-        System.out.println("2.t1.level : " + t1.level + ",t2.level :" + t2.level );
+        System.out.println("2.t1.level : " + t1.level + ",t2.level :" + t2.level);
     }
 
 }
