b7d7b4428a505ac9a8976a242f6ce81f4b1c46e6
koniu <dupa>
2018-01-10 19:14:12 +0100

    rozdział trzeci 92.

--- ThinkingInJava/src/main/java/ThinkingInJava/operators94.java
+++ ThinkingInJava/src/main/java/ThinkingInJava/operators94.java
@@ -3,7 +3,7 @@ package ThinkingInJava;
 import java.util.Date;
 
 
-public class operatory94 {
+public class operators94 {
     public static void main (String[] arqs){
         System.out.println("Witaj , dzisiaj jest : ");
         System.out.println(new Date());
