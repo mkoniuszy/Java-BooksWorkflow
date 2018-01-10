c450da313c2bc7415e560717bec8da0d4a0c5dfa
koniu <dupa>
2018-01-10 16:21:11 +0100

    rozdział Pierwszy 92.

---
+++ ThinkingInJava/src/main/java/ThinkingInJava/operatory94.java
@@ -0,0 +1,11 @@
+package ThinkingInJava;
+
+import java.util.Date;
+
+
+public class operatory94 {
+    public static void main (String[] arqs){
+        System.out.println("Witaj , dzisiaj jest : ");
+        System.out.println(new Date());
+    }
+}
